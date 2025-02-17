c7925da2301398a734c4e558ac1921117666cb8e
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-02-17 10:27:19 +0530

    Create Experiment_2.py

---
+++ Experiment_2.py
@@ -0,0 +1,15 @@
+# Experiment 2: Use of input statements, operators
+'''
+1. Declare these variables (x, y and z) as integers. Assign a value of 9 to x, Assign 
+a value of 7 to y, perform addition, multiplication, division and subtraction on 
+these two variables and Print out the result.
+'''
+
+x = 9
+y = 7
+z=0
+
+print("x + y", x + y)
+print("x * y", x * y)
+print("x / y", x / y)  # For float division
+print("x - y", x - y)
