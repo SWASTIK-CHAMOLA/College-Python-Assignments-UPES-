213ea1afc240ebfa883dda4b76d2a3f83b3bc565
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-02-17 10:25:54 +0530

    Create Experiment_1.py

---
+++ Experiment_1.py
@@ -0,0 +1,15 @@
+# EXPERIMENT-1: Python Installation and starting with python
+'''
+ 2. Write Python programs to print strings in the given manner: 
+a)   Hello Everyone !!! 
+b)   Hello 
+      World 
+c)   Hello 
+              World 
+d) ‘ Rohit’ s date of birth is 12\05\1999’ 
+'''
+print("\nSWASTIK CHAMOLA\nBATCH - 51\nSAP ID: 590016735\n")
+print("HELLO EVERYONE !!")
+print("HELLO\nWORLD ")
+print("HELLO\n        WORLD ")
+print("Rohit’ s date of birth is 12\05\1999")
