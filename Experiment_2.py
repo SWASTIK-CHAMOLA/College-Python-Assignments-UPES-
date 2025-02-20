4eff2286f30aad02582c1fb5e5f5f81a18a21d5e
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-02-20 07:58:02 +0530

    Update Experiment_2.py

--- Experiment_2.py
+++ Experiment_2.py
@@ -25,3 +25,10 @@ Expected output: 49 '''
 x = int(input("enter value of x: "))
 y = int(input("enter value of y: "))
 print("output: ",{(x+y)*(x+y)})
+
+''' 4. Write a program to compute the length of the hypotenuse (c) of a right triangle using Pythagoras theorem.'''
+
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+c = (a**2 + b**2)**0.5
+print("The length of the hypotenuse is: ",c)
