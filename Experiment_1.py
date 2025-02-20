56113090881d801497ee02e3afcb6414e0db47e1
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-02-20 07:56:36 +0530

    Update Experiment_1.py

--- Experiment_1.py
+++ Experiment_1.py
@@ -43,3 +43,37 @@ after adding your First name and Last name together. '''
 f = "SWASTIK"
 l = "CHAMOLA"
 print(f,"",l )
+
+'''  6   Declare three variables, consisting of your first name, your last name and Nickname.
+Write a  program that prints out your first name, then your nickname in parenthesis and then your last name.   
+Example output : George ( woody ) Washington. '''
+g = input("enter your first name")
+m = input("enter your nickname")
+gm = input("enter your last name")
+print(g , " (" , m , ") " , gm)
+
+'''  7 Declare and assign values to suitable variables and print in the following way : 
+NAME : NIKUNJ BANSAL   
+SAP ID : 500069944 
+DATE OF BIRTH : 13 Oct 1999 
+ADDRESS : UPES 
+                    Bidholi Campus 
+                    Pincode : 248007 
+Programme : AI & ML                                                                                       
+Semester : 2 '''
+q = input("enter your name")
+print("NAME : ",q)
+B = int(input("enter your SAP ID: "))
+print("SAP ID: : ",B)
+Q = input("enter your D.O.B")
+print("D.O.B : ",Q)
+address = [
+    "ADDRESS : UPES",
+    "Bidholi Campus",
+    "Pincode : 248007",
+    "Programme : AI & ML",
+    "Semester : 2"
+]
+
+for i in address:
+    print(i)
