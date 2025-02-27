a06cdc78f85d9b81ea62ea01aa933e183046e0ef
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-02-27 16:35:55 +0530

    Update Experiment4.py

--- Experiment4.py
+++ Experiment4.py
@@ -1,1 +1,10 @@
+                                #Experiment 4: Loops
+
+'''1. Find a factorial of given number. '''
+
+q = int(input("Enter the no you want to find factorial of: "))
+factorial = 1
+for i in range(q,1,-1):
+    factorial = factorial * i
+print(factorial)
 
