82ce2a51cd79ddb8087a04adfc728a8249b9c235
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-02-28 07:39:47 +0530

    Update Experiment4.py

--- Experiment4.py
+++ Experiment4.py
@@ -8,3 +8,12 @@ for i in range(q,1,-1):
     factorial = factorial * i
 print(factorial)
 
+'''2. Find whether the given number is Armstrong number. '''
+num = int(input("Enter a number: "))  
+sum = 0  
+temp = num  
+while temp > 0:  
+    digit = temp % 10  
+    sum += digit ** len(str(num))  
+    temp //= 10  
+print(f"{num} is an Armstrong number." if num == sum else f"{num} is not an Armstrong number.")
