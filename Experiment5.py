d0664056a8a129b497082c00bc7f17a83fb5295e
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2025-03-15 08:34:27 +0530

    Update Experiment5.py

--- Experiment5.py
+++ Experiment5.py
@@ -1,1 +1,15 @@
-# EXPERIMENT 5
+                                      # Experiment 5: String and Sets
+
+'''1.  Write a program to count and display the number of capital letters in a given string. '''
+
+o = input("Enter a string: ")
+for i in o:
+  if i in i.capitalize():
+      print(i, end=' ')
+
+'''2.  Count total number of vowels in a given string.'''
+
+o = input("Enter a string: ")
+for i in o:
+  if i in 'aeiou':
+      print(i, end=' ')
